e70622bb00d1843394655c122e4a4e5f9950f2ab
Dansou lionel josue <lioneldansou222@gmail.com>
2026-04-29 21:30:09 +0000

    ecriture du code des classe Contact et ContactsManager

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,4 @@
 public class Contact {
+    String nom;
+    String Numero_de_telephone;
 }
--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,2 +1,26 @@
 public class ContactsManager {
+    Contact[] mes_amis;
+    int Compteur;
+
+    // Constructeur par défaut
+    public ContactsManager() {
+        this.Compteur = 0;
+        this.mes_amis = new Contact[500];
+    }
+
+    // Méthode pour ajouter un contact
+    public void addContact(Contact contact) {
+        mes_amis[Compteur] = contact;
+        Compteur++;
+    }
+
+    // Méthode pour rechercher un contact par nom
+    public Contact searchContact(String searchName) {
+        for (int i = 0; i < Compteur; i++) {
+            if (mes_amis[i].nom.equals(searchName)) {
+                return mes_amis[i];
+            }
+        }
+        return null;
+    }
 }
